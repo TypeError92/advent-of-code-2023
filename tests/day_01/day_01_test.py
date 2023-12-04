78bf2e811c3703e7df5373b31a4092b25cc9aed0
typeerror92 <simonwalgenbach@posteo.de>
2023-12-04 01:18:30 +0000

    move everything into day_01

--- tests/day_01/day_01_test.py
+++ tests/day_01/day_01_test.py
@@ -2,4 +2,5 @@ from day_01.day_01 import calibrate_trebuchet
 
 
 def test_day_01():
-    assert calibrate_trebuchet("example.txt") == 142
+    assert calibrate_trebuchet("example1.txt") == 142
+    assert calibrate_trebuchet("example2.txt") == 281
